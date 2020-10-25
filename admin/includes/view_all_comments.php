9247f93a2e85c90ac3e2c7d89d1cbd71a58cc33f
olteanuflorin86 <olteanuflorin86@gmail.com>
2020-10-25 17:05:48 +0200

    Can now add a comment from post.php form

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -2,15 +2,17 @@
                         	<thead>
                         		<tr>
                         			<th>Id</th>
-                        			<th>Category id</th>
-                        			<th>Title</th>                       			
-                        			<th>Author</th>                       			
-                        			<th>Date</th>
-                        			<th>Image</th>
-                        			<th>Content</th>
-                        			<th>Tags</th>
-                        			<th>Comments</th>
+                        			<th>Author</th>  
+                        			<th>Comments</th>                        			
+                        			<th>Email</th>
                         			<th>Status</th>
+                        			<th>In Response to</th>                        			
+                        			<th>Date</th>
+                        			<th>Approve</th>
+                        			<th>Unapprove</th>
+                        			<th>Edit</th>
+                        			<th>Delete</th>
+                        			
                         		</tr>                        	
                         	</thead>
                         	<tbody>
@@ -18,58 +20,55 @@
                         		<?php 
                         		
                         		// FIND ALL POSTS QUERY
-                        		$query = 'SELECT * FROM posts';
-                        		$select_posts = mysqli_query($connection, $query);
+                        		$query = 'SELECT * FROM comments';
+                        		$select_comments = mysqli_query($connection, $query);
                         		
-                        		if(!$select_posts) {
+                        		if(!$select_comments) {
                         		    
                         		    die("QUERY FAILURE" . mysqli_error($connection));
                         		    
                         		}
                         		
-                        		while($row = mysqli_fetch_assoc($select_posts)) {
-                        		    $post_id = $row['post_id']; 
-                        		    $post_author = $row['post_author'];
-                        		    $post_title = $row['post_title'];
-                        		    $post_category_id = $row['post_category_id'];
-                        		    $post_status = $row['post_status']; 
-                        		    $post_image = $row['post_image'];
-                        		    $post_tags = $row['post_tags'];
-                        		    $post_comment_count = $row['post_comment_count'];
-                        		    $post_date = $row['post_date'];
-                        		    $post_content = $row['post_content'];
-
+                        		while($row = mysqli_fetch_assoc($select_comments)) {
+                        		    $comment_id = $row['comment_id']; 
+                        		    $comment_post_id = $row['comment_post_id'];
+                        		    $comment_author = $row['comment_author'];
+                        		    $comment_content = $row['comment_content']; 
+                        		    $comment_email = $row['comment_email'];
+                        		    $comment_status = $row['comment_status'];
+                        		    $comment_date = $row['comment_date'];
                         		    
-                        		    ?>
-                        		    
-                        		    <?php 
-                        		    
-                        		      $query = "SELECT cat_title FROM categories WHERE cat_id={$post_category_id}";
-                        		      $select_category_title = mysqli_query($connection, $query);
-                        		      confirmQuery($select_category_title);
-                        		      
-                        		      while($row = mysqli_fetch_assoc($select_category_title)) {
-                        		          $cat_title_name = $row['cat_title'];
-                        		      }
-                        		    
-                        		    ?>
+                        		?>
                         		    
                         		    
                         		    <tr>
-                            		    <td><?php echo $post_id; ?></td>
+                            		    <td><?php echo $comment_id; ?></td>
                             		    <!--<td><?php //echo $post_category_id; ?></td>-->
-                            		    <td><?php echo $cat_title_name; ?></td>                            		    
+                            		    <td><?php echo $comment_author; ?></td>                          		    
+                            		    <td><?php echo $comment_content; ?></td>
+                            		    <td><?php echo $comment_email; ?></td>
+                            		    <td><?php echo $comment_status; ?></td>
                             		    
-                            		    <td><?php echo $post_title; ?></td>
-                            		    <td><?php echo $post_author; ?></td>
-                            		    <td><?php echo $post_date; ?></td>
-                            		    <td><img src="../images/<?php echo $post_image; ?>" width="200" height="64" alt="post_image" class="img-responsive"/></td>
-                            		    <td><?php echo $post_content; ?></td>
-                            		    <td><?php echo $post_tags; ?></td>
-                            		    <td><?php echo $post_comment_count; ?></td>
-                            		    <td><?php echo $post_status; ?></td>
-                            		    <td><a href=<?php echo "posts.php?source=edit_post&p_id={$post_id}"; ?>>Edit</a></td>
-                            		    <td><a href=<?php echo "posts.php?delete={$post_id}"; ?>>Delete</a></td>
+                            		    <?php 
+                        		    
+                            		      $query = "SELECT * FROM posts WHERE post_id={$comment_post_id}";
+                            		      $select_comment_post_title = mysqli_query($connection, $query);
+                            		      confirmQuery($select_comment_post_title);
+                            		      
+                            		      while($row = mysqli_fetch_assoc($select_comment_post_title)) {
+                            		          $post_id = $row['post_id'];
+                            		          $comment_post_title = $row['post_title'];
+                            		      }
+                            		    
+                            		    ?>  
+                            		    <!-- If we click a link we will go to the specific post -->                          		    
+                            		    <td><a href="../post.php?p_id=<?php echo $post_id; ?>"><?php echo $comment_post_title; ?></a></td>
+                            		    
+                            		    <td><?php echo $comment_date; ?></td>
+                            		    <td><a href=<?php echo "posts.php?source=edit_post&p_id="?>>Approve</a></td>
+                        				<td><a href=<?php echo "post.php?delete="?>>Unapprove</a></td>
+                        				<td><a href=<?php echo "posts.php?source=edit_post&p_id="?>>Edit</a></td>
+                        				<td><a href=<?php echo "post.php?delete="?>>Delete</a></td>
                         			</tr>
                         		<?php } ?>                		
                         		
--- post.php
+++ post.php
@@ -23,6 +23,7 @@
                 </h1>                
             
                 <?php 
+                // We show the post with a certain id
                 
                 if(isset($_GET['p_id'])) {
                     
@@ -56,17 +57,51 @@
 				<hr>
 				
 				<?php }} ?>
+				
+				<?php 
+				// We get and post a comment
+				
+				if(isset($_POST['create_comment'])) {
+				    
+				    $the_p_id = $_GET['p_id'];
+				    $comment_author = $_POST['comment_author'];
+				    $comment_email = $_POST['comment_email'];
+				    $comment_content = $_POST['comment_content'];
+				    
+				    $query = "INSERT INTO comments".
+								    "(comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ".
+								    "VALUES({$the_p_id}, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'Approved', now())";
+				    
+				    $add_comment_query = mysqli_query($connection, $query);
+				    confirmQuery($add_comment_query);
+				    
+				}
+				
+				
+				?>
 
                 <!-- Blog Comments -->
 
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                    <form role="form" action="" method="post">
+                    
+                    	<div class="form-group">
+                    		<label for="comment_author">Author</label>
+                            <input type="text" class="form-control" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        
+                        <div class="form-group">
+                        	<label for="comment_email">Email</label>
+                            <input type="email" class="form-control" name="comment_email">
+                        </div>
+                    
+                        <div class="form-group">
+                        	<label for="comment_content">Your Comment</label>
+                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
                     </form>
                 </div>
 

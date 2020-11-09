4fe65c2b9d21a83918f51f07c25a71bc5c8af478
olteanuflorin86 <olteanuflorin86@gmail.com>
2020-11-09 14:37:30 +0200

    Initial validation of users based on roles after they go through login

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -5,6 +5,23 @@
 
 <?php session_start(); ?>
 
+<?php 
+
+if(isset($_SESSION['user_role'])) {
+    
+    if($_SESSION['user_role'] == 'subscriber') {
+        
+        header('Location: ../index.php');
+        
+    } else if ($_SESSION['user_role'] == 'admin') {
+        // things will go through as intended ... we can skip this else if, of course...         
+    }
+    
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
  
